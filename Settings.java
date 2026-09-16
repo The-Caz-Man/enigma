8bac73a05c7f583606fd3fe8e39dc56dec02feab
Caz Cherepak <m.caz.cherepak@gmail.com>
2026-09-16 18:47:07 -0500

    Created variables in Settings.java to represent all of the possible settings

--- Settings.java
+++ Settings.java
@@ -3,19 +3,24 @@ public class Settings {
 
         //Which reflector
             //Example: B
+        char reflector;
     
         //Which 3 of the 5 rotors in which order
             //Example: II, V, IV
+        String [] rotors = new String[3];
 
         //Ring setting of each rotor. Number to letter to turn notch.
             //Example: 2, 21, 12
                 //(II, set a to 2), (V, set a to 21), (IV, set a to 12)
+        int [] ringSettings = new int[3];
 
         //Starting positions of rotors. Which number is it starting on.
-            //Example: W, G, L
+            //Example: 3, 24, 2
+        int [] rotorPositions = new int[3];
 
         //Plug board configurations
             //Example: AV BS CG DL FU HZ IN KM OW RX
+        String [] plugConfiguration = new String[10];
 
 }
 
