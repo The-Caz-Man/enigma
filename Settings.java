2927ce583b1e4e0613d0984b17ac1357a889fb00
Caz Cherepak <m.caz.cherepak@gmail.com>
2026-09-16 18:35:06 -0500

    Added better comments for Settings class

--- Settings.java
+++ Settings.java
@@ -1,12 +1,21 @@
 public class Settings {
+    //ATTRIBUTES
+
+        //Which reflector
+            //Example: B
     
-    //Which 3 of the 5 rotors in which order
+        //Which 3 of the 5 rotors in which order
+            //Example: II, V, IV
 
-    //Ring setting of each rotor. Number to letter to turn notch.
+        //Ring setting of each rotor. Number to letter to turn notch.
+            //Example: 2, 21, 12
+                //(II, set a to 2), (V, set a to 21), (IV, set a to 12)
 
-    //Starting positions of rotors. Which number is it starting on.
+        //Starting positions of rotors. Which number is it starting on.
+            //Example: W, G, L
 
-    //Plug board configurations
+        //Plug board configurations
+            //Example: AV BS CG DL FU HZ IN KM OW RX
 
 }
 
