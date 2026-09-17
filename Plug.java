6d64f75234a4e33f7823058e496b770885cefcd2
Caz Cherepak <m.caz.cherepak@gmail.com>
2026-09-16 19:15:15 -0500

    Made plug class and inOut method.

--- Plug.java
+++ Plug.java
@@ -1,11 +1,27 @@
 //Represents one of the plugs found on the plug board. Up to 10.
 public class Plug {
     //ATTRIBUTES
+    //letter connections. Swap A and G, for example. ("AG")
+    String connections;
 
-        //letter connections. Swap A and G, for example.
+    //plug constructor
+    public Plug(String connections) {
+        this.connections = connections;
+    }
 
-    //METHODS
+    //PRE: Takes Char matching a char in connections
+    //POST: return other char
+    public char inToOut(char input) {
 
-        //Input to output
+        int outputIndex;
+        int inputIndex = this.connections.indexOf(input);
+        if (inputIndex == 0)
+            outputIndex = 1;
+        else
+            outputIndex = 0;
+        char output = this.connections.charAt(outputIndex);
+        return output;
+        
+    }
 
 }
