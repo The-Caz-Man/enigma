6574e4ec0ee8d6fa7d8163327e47b7bdb6f2f62e
Caz Cherepak <m.caz.cherepak@gmail.com>
2026-09-16 20:49:46 -0500

    Changed Settings to more accurately represent the real machine

--- Settings.java
+++ Settings.java
@@ -1,18 +1,9 @@
 public class Settings {
     //ATTRIBUTES
-
-        //Which reflector
-            //Example: B
-        char reflector;
     
         //Which 3 of the 5 rotors in which order
             //Example: II, V, IV
         String [] rotors = new String[3];
-
-        //Ring setting of each rotor. Number to letter to turn notch.
-            //Example: 2, 21, 12
-                //(II, set a to 2), (V, set a to 21), (IV, set a to 12)
-        int [] ringSettings = new int[3];
 
         //Starting positions of rotors. Which number is it starting on.
             //Example: 3, 24, 2
@@ -23,10 +14,8 @@ public class Settings {
         String [] plugConfiguration = new String[10];
 
         //Settings constructor
-        public Settings(char reflector, String[] rotors, int[] ringSettings, int[] rotorPositions, String[] plugConfiguration) {
-            this.reflector = reflector;
+        public Settings(String[] rotors, int[] rotorPositions, String[] plugConfiguration) {
             this.rotors = rotors;
-            this.ringSettings = ringSettings;
             this.rotorPositions = rotorPositions;
             this.plugConfiguration = plugConfiguration;
         }
