8578005b3922b554ffe8eb51b9b3c44c851accca
Caz Cherepak <m.caz.cherepak@gmail.com>
2026-09-16 21:47:40 -0500

    Cleaned up some comments in Settings

--- Settings.java
+++ Settings.java
@@ -1,24 +1,24 @@
 public class Settings {
     //ATTRIBUTES
     
-        //Which 3 of the 5 rotors in which order
-            //Example: II, V, IV
-        String [] rotors = new String[3];
+    //Which 3 of the 5 rotors in which order
+    //Example: II, V, IV
+    String [] rotors = new String[3];
 
-        //Starting positions of rotors. Which number is it starting on.
-            //Example: 3, 24, 2
-        int [] rotorPositions = new int[3];
+    //Starting positions of rotors. Which number is it starting on.
+    //Example: 3, 24, 2
+    int [] rotorPositions = new int[3];
 
-        //Plug board configurations
-            //Example: AV BS CG DL FU HZ IN KM OW RX
-        String [] plugConfiguration = new String[10];
+    //Plug board configurations
+    //Example: AV BS CG DL FU HZ IN KM OW RX
+    String [] plugConfiguration = new String[10];
 
-        //Settings constructor
-        public Settings(String[] rotors, int[] rotorPositions, String[] plugConfiguration) {
-            this.rotors = rotors;
-            this.rotorPositions = rotorPositions;
-            this.plugConfiguration = plugConfiguration;
-        }
+    //Settings constructor
+    public Settings(String[] rotors, int[] rotorPositions, String[] plugConfiguration) {
+        this.rotors = rotors;
+        this.rotorPositions = rotorPositions;
+        this.plugConfiguration = plugConfiguration;
+    }
 
 }
 
